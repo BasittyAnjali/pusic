e2dde54705d3883e0fb250d27db8749b615d8d62
Basitty Anjali <basittyanjali11200@gmail.com>
2022-10-24 11:20:01 +0530

    Create music.js

---
+++ src/db/music.js
@@ -0,0 +1,544 @@
+let musicDB = [{
+        id: 0,
+        name: "Find A Way (feat. Cammie Robinson)",
+        author_name: "T & Sugah x NCT",
+        img: "FindAWay.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "T & Sugah x NCT - Find A Way (feat. Cammie Robinson) [NCS Release].mp3",
+        attribution: {
+            song: "T & Sugah x NCT - Find A Way (feat. Cammie Robinson) [NCS Release",
+            musicBy: "NoCopyrightSounds",
+            download: "https://ncs.io/FindAWay",
+            stream: "https://youtu.be/ylbkAiBqUBc"
+        }
+    },
+    {
+        id: 1,
+        name: "Dreamatic",
+        author_name: "iFeature",
+        img: "Dreamatic.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "instrumental",
+        musicName: "iFeature - Dreamatic [NCS Release].mp3",
+        attribution: {
+            song: "iFeature - Dreamatic [NCS Release]",
+            musicBy: "mixkit",
+            download: "https://ncs.io/Dreamatic",
+            stream: "https://youtu.be/B6HQX1sAkqA"
+        }
+    },
+    {
+        id: 2,
+        name: "OverMyHead",
+        author_name: " ROY KNOX",
+        img: "over-my-head-1602154826-mOiKzOJDe6.jpg",
+        lang: "ENGLISH",
+        type: "electronic",
+        timesPlayed: 0,
+        musicName: "ROY KNOX -  Over My Head (Feat. Mike Robert) [NCS Release].mp3",
+        attribution: {
+            song: "ROY KNOX - Over My Head (Feat. Mike Robert) [NCS Release]",
+            musicBy: "NoCopyrightSounds",
+            download: "http://ncs.io/OverMyHead",
+            stream: null
+        }
+    },
+    {
+        id: 3,
+        name: "Want You (feat. Sara Skinner)",
+        author_name: "Subtact, Sara Skinner",
+        img: "want-you-feat-sara-skinner-1586956276-s4lOrfrHQj.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "Subtact - Want You (feat. Sara Skinner) [NCS Release].mp3",
+        attribution: {
+            song: "Subtact - Want You (feat. Sara Skinner) [NCS Release]",
+            musicBy: "NoCopyrightSounds",
+            download: "http://ncs.io/WantYou",
+            stream: "http://youtu.be/Pww31vN_1QY"
+        }
+    },
+    {
+        id: 4,
+        name: "Crying for No Reason",
+        author_name: "Katy B",
+        img: "Reason.png",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "Midranger - Apocalypse [NCS Release].mp3",
+        attribution: {
+            song: "Midranger - Apocalypse [NCS Release]",
+            musicBy: "NoCopyrightSounds",
+            download: "http://ncs.io/Apocalypse",
+            stream: "http://youtu.be/Whw1sWDpAvE"
+        }
+    },
+    {
+        id: 5,
+        name: "Alive",
+        author_name: "Neoni, N3WPORT",
+        img: "alive.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "N3WPORT - Alive (feat. Neoni) [NCS Release].mp3",
+        attribution: {
+            song: "N3WPORT - Alive (feat. Neoni) [NCS Release]",
+            musicBy: "NoCopyrightSounds",
+            download: "http://ncs.io/NAlive",
+            stream: "http://youtu.be/vJAcXd_UtWQ"
+        }
+    },
+    {
+        id: 6,
+        name: "Impact Moderato",
+        author_name: "Kevin MacLeod",
+        img: "notAvailable.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "instrumental",
+        musicName: "impact.mp3",
+        attribution: {
+            song: "Impact Moderato",
+            musicBy: "Kevin MacLeod",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 7,
+        name: "Impact Moderato 2",
+        author_name: "Kevin MacLeod",
+        img: "notAvailable.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "instrumental",
+        musicName: "impact2.mp3",
+        attribution: {
+            song: "Impact Moderato",
+            musicBy: "Kevin MacLeod",
+            download: null,
+            stream: null
+        }
+    },
+    {
+        id: 8,
+        name: "Ignite",
+        author_name: "Tom wilson, Heuse",
+        img: "ignite-1600419626-nHn4WXY79q.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3",
+        attribution: {
+            song: "Heuse & Tom Wilson - Ignite [NCS Release]",
+            musicBy: "NoCopyrightSounds",
+            download: "",
+            stream: "http://youtu.be/zbqgv0mMfwk"
+        }
+    },
+    {
+        id: 9,
+        name: "Let Me Love You",
+        author_name: "Justin Bieber",
+        img: "LetMeLoveYou.jpg",
+        lang: "ENGLISH",
+        timesPlayed: 0,
+        type: "electronic",
+        musicName: "Let_Me_Love_You.mp3",
+        attribution: {
+            song: "Let Me Love You",
+            musicBy: "NoCopyrightSounds",
+            download: "http://ncs.io/Ignite",
+            stream: "http://youtu.be/zbqgv0mMfwk"
+        }
+    },
+    {
+        id: 10,
+        name: "Bezubaan Phir Se",
+        author_name: "Amit Trivedi",
+        img: "Bezubaan Phir Se.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Bezubaan_Phir_Se.mp3"
+    },
+    {
+        id: 11,
+        name: "Ra Ra Rakkamma",
+        author_name: "Mangli and Nakash Aziz",
+        img: "Rakkamma.jpg",
+        lang: "TELUGU",
+        timesPlayed: 0,
+        musicName: "Ra Ra Rakkamma.mp3"
+    },
+    {
+        id: 12,
+        name: "Ranjha",
+        author_name: "Jasleen Royal, B Praal, and Romy",
+        img: "Ranjha.jpg",
+        lang: "PUNJABI",
+        timesPlayed: 0,
+        musicName: "Ranjha.mp3"
+    },
+    {
+        id: 13,
+        name: "Varaha Roopam",
+        author_name: "Sai Vignesh",
+        img: "14134_4.jpg",
+        lang: "Kannada",
+        timesPlayed: 0,
+        musicName: "Varaha Roopam.mp3"
+    },
+    {
+        id: 14,
+        name: "Genda Phool",
+        author_name: "B praak and Payal Dev",
+        img: "notAvailable.jpg",
+        lang: "BENGALI",
+        timesPlayed: 0,
+        musicName: "Genda Phool.mp3"
+    },
+    {
+        id: 15,
+        name: "Butta Bomma",
+        author_name: "Armaan Malik",
+        img: "butta bomma.jfif",
+        lang: "TELUGU",
+        timesPlayed: 0,
+        musicName: "Butta Bomma.mp3"
+    },
+    {
+        id: 16,
+        name: "Chann Sitare",
+        author_name: "Ammy Virk",
+        img: "chann sitare.jfif",
+        lang: "PUNJABI",
+        timesPlayed: 0,
+        musicName: "Chann Sitare.mp3"
+    },
+    {
+        id: 17,
+        name: "Yatta Yatta Yatta",
+        author_name: "Roopam Kumar",
+        img: "12702_4.jpg",
+        lang: "KANNADA",
+        timesPlayed: 0,
+        musicName: "Yatta Yatta Yatta.mp3"
+    },
+    {
+        id: 18,
+        name: "Mere Sohneya",
+        author_name: "Sachet and Parampara",
+        img: "Mere Sohneya.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Mere Sohneya.mp3"
+    },
+    {
+        id: 19,
+        name: "Ee-Raathale",
+        author_name: "Yuvan Shankar Raj",
+        img: "Ee-Raathale.jpg",
+        lang: "TELUGU",
+        timesPlayed: 0,
+        musicName: "Ee-Raathale.mp3"
+    },
+    {
+        id: 20,
+        name: "Dil Tod Ke",
+        author_name: "B Praak",
+        img: "Dil Tod Ke.jpg",
+        lang: "PUNJABI",
+        timesPlayed: 0,
+        musicName: "Dil Tod Ke.mp3"
+    },
+    {
+        id: 21,
+        name: "Love Me Like You Do",
+        author_name: "Ellie_Goulding",
+        img: "lovemelikeyoudo.jpg",
+        lang: "English",
+        timesPlayed: 0,
+        musicName: "Love Me Like You Do.mp3"
+    },
+    {
+        id: 22,
+        name: "Lagdi Lahore Di",
+        author_name: "Guru Randhawa and Tulsi Kumar",
+        img: "Lagdi Lahore Di.jpg",
+        lang: "PUNJABI",
+        timesPlayed: 0,
+        musicName: "Lagdi Lahore Di.mp3"
+    },
+    {
+        id: 23,
+        name: "Naatu-Naatu",
+        author_name: "Rahul Siplingunj",
+        img: "Naatu-Naatu.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Naatu-Naatu.mp3"
+    },
+    {
+        id: 24,
+        name: "Sittharala Sirapadu",
+        author_name: "Sachet and Parampara",
+        img: "sittarala-sirapadu.webp",
+        lang: "TELUGU",
+        timesPlayed: 0,
+        musicName: "Sittharala Sirapadu.mp3"
+    },
+    {
+        id: 25,
+        name: "Heer",
+        author_name: "Harshdeep Kaur",
+        img: "Heer.jpg",
+        lang: "PUNJABI",
+        timesPlayed: 0,
+        musicName: "Heer.mp3"
+    },
+    {
+        id: 26,
+        name: "Namo Namo",
+        author_name: "Amit Trivedi",
+        img: "Namo Namo.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Namo Namo.mp3"
+    },
+    {
+        id: 27,
+        name: "Illegal Weapon 2.0",
+        author_name: "Jasmine Sandlas",
+        img: "4333_4.jpg",
+        lang: "PUBJABI",
+        timesPlayed: 0,
+        musicName: "Illegal-Weapon-2.0.mp3"
+    },
+    {
+        id: 28,
+        name: "Khairiyat",
+        author_name: "Arijit Singh",
+        img: "2817_4.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Khairiyat.mp3"
+    },
+    {
+        id: 29,
+        name: "Mere Liye Tum Kaafi Ho (From \"Shubh Mangal Zyada Saavdhan\")",
+        author_name: "Ayushmann Khurrana",
+        img: "4351_4.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Mere Liye Tum Kaafi Ho.mp3"
+    },
+    {
+        id: 30,
+        name: "Yaar Mod Do",
+        author_name: "Guru Randhawa",
+        img: "8683_4.jpg",
+        lang: "PUNJABI",
+        timesPlayed: 0,
+        musicName: "Yaar ModDo.mp3"
+    },
+    {
+        id: 31,
+        name: "Shayad",
+        author_name: "Pritam, Arijit Singh",
+        img: "4342_4.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Shayad.mp3"
+    },
+    {
+        id: 32,
+        name: "Tareefan",
+        author_name: "Badshah",
+        img: "Tarefaan.jpg",
+        lang: "HINDI",
+        timesPlayed: 0,
+        musicName: "Tareefan.mp3"
+    }
+];
+
+export default musicDB;
+/*let musicDB = [
+  {
+    id:0,
+    name:"Left Foot Up Right Foot Slide",
+    author_name: "Yung Nazty",
+    img: "left_foot_up_right_foot_slide.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"left_foot_up_right_foot_slide.mp3"
+  },
+  {
+    id:1,
+    name: "Intentions (feat. Quavo)",
+    author_name: "Justin Bieber",
+    img: "intentions.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"intentions.mp3"
+  },
+  {
+    id:3,
+    name: "Señorita",
+    author_name: "Shawn Mendes",
+    img: "senorita.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"senorita.mp3"
+  },
+  {
+    id:4,
+    name: "Memories",
+    author_name: "Maroon 5",
+    img: "memories.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"memories.mp3"
+  },
+  {
+    id:5,
+    name: "Skechers",
+    author_name: "DripReport",
+    img: "skechers.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"Skechers.mp3"
+  },
+  {
+    id:6,
+    name: "Sunflower - Spider-Man: Into the Spider-Verse",
+    author_name: "Post Malone",
+    img: "sunflower.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"Sunflower.mp3"
+  },
+  {
+    id:7,
+    name: "I Can’t Get Enough",
+    author_name: "benny blanco, Selena Gomez, J Balvin, Tainy",
+    img: "i_cant_get_enough.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"i_cant_get_enough.mp3"
+  },
+  {
+    id:8,
+    name: "Old Town Road",
+    author_name: "Lil Nas X",
+    img: "old_town_road.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"old_town_road.mp3"
+  },
+  {
+    id:9,
+    name: "Beautiful Stillness",
+    author_name: "Billy-Joe",
+    img: "beautiful_stillness.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"Beautiful_Stillness.mp3"
+  },
+  {
+    id:10,
+    name: "death bed (coffee for your head)",
+    author_name: "Powfu",
+    img: "death_bed.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"death_bed.mp3"
+  },
+  {
+    id:11,
+    name: "Halka Halka (From \"Fanney Khan\")",
+    author_name: "Sunidhi Chauhan",
+    img: "halka_halka_suroor.jpg",
+    lang:"HINDI",
+    timesPlayed:0,
+    musicName:"halka_halka_suroor.mp3"
+  },
+  {
+    id:12,
+    name: "Kudi Nu Nachne De (From \"Angrezi Medium\")",
+    author_name: "Vishal Dadlani",
+    img: "kudi_nu_nachne_de.jpg",
+    lang:"HINDI",
+    timesPlayed:0,
+    musicName:"kudi_nu_nachne_de.mp3"
+  },
+  {
+    id:17,
+    name: "Without Me",
+    author_name: "Halsey",
+    img: "without_me.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"without_me.mp3"
+  },
+  {
+    id:18,
+    name: "Buttabomma - Telugu",
+    author_name: "Armaan Malik",
+    img: "buttabomma.jpg",
+    lang:"TELUGU",
+    timesPlayed:0,
+    musicName:"ButtaBomma.mp3"
+  },
+  {
+    id:19,
+    name: "Coda",
+    author_name: "Jakob Ahlbom",
+    img: "coda.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"coda.mp3"
+  },
+  {
+    id:23,
+    name: "Fracture",
+    author_name: "Jakob Ahlbom",
+    img: "fracture.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"fracture.mp3"
+  },
+  {
+    id:25,
+    name: "Happier",
+    author_name: "Marshmello",
+    img: "happier.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"happier.mp3"
+  },
+  {
+    id:27,
+    name: "Faded",
+    author_name: "Alan Walker",
+    img: "faded.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"Faded_Alan_Walker.mp3"
+  },
+  {
+    id:28,
+    name: "I Don't Care (with Justin Bieber)",
+    author_name: "Ed Sheeran",
+    img: "i_dont_care.jpg",
+    lang: "ENGLISH",
+    timesPlayed:0,
+    musicName:"i_dont_care.mp3"
+  }
+]
+export default musicDB;*/
